f6fa06c6c6114430f6d5a0ea9084959ea220487b
kaho0 <kahonbintezaman@gmail.com>
2024-05-25 21:58:56 +0600

    powers

--- binary/getNthBit.cpp
+++ binary/getNthBit.cpp
@@ -20,10 +20,22 @@ int clearIthBit(int num,int i)
     int bitmask=~(1<<i);
     return num&bitmask;
 }
+bool isPowerOfTwo(int num)
+{
+    if(!(num&(num-1)))
+    {
+        return true;
+    }
+    else return false;
+}
 int main()
 {
     cout<<getNthBit(6,2)<<endl; 
     cout<<getNthBit(7,5)<<endl;
     cout<<setIthBit(6,3)<<endl;
-    cout<<clearIthBit(6,1);
+    cout<<clearIthBit(6,1)<<endl;
+    cout<<isPowerOfTwo(4)<<endl;
+    cout<<isPowerOfTwo(5)<<endl;
+    cout<<isPowerOfTwo(2)<<endl;
+    cout<<isPowerOfTwo(11)<<endl;
 }
