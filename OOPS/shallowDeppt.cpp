b9e12d2aecfcf2447133752eed973fb67ea0a25e
kaho0 <kahonbintezaman@gmail.com>
2024-06-03 22:56:16 +0600

    milage

--- OOPS/shallowDeppt.cpp
+++ OOPS/shallowDeppt.cpp
@@ -1,2 +1,32 @@
 #include<iostream>
 using namespace std;
+class car{
+    string name;
+    string color;
+    int *milage;
+car(string name,string color)
+{
+  this->name=name;
+  this->color=color;
+
+milage=new int;
+*milage=12;
+}
+
+car(car &original)
+{
+    name=original.name;
+    color=original.color;
+    milage=new int;
+    milage=original.milage;
+}
+};
+int main()
+{
+    car c1("maruti 800","blue");
+    car c2(c1);
+    cout<<c2.name<<endl;
+    cout<<c2.color<<endl;
+    cout<<*c2.milage<<endl;
+
+}
